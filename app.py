ade3f34fba56bfda1f1b755f82117acdd00894bb
SaraRebecca <sara.rebecca@studenti.unipd.it>
2026-06-08 14:41:00 +0200

    add Expenses model and initialize database

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Transaction Database Configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
+db = SQLAlchemy(app)
+class Expenses(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    category = db.Column(db.Text, nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    beneficiary = db.Column(db.Text, nullable=False)
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
+with app.app_context():
+    db.create_all()
+    
 if __name__ == "__main__":
     app.run(debug=True)
